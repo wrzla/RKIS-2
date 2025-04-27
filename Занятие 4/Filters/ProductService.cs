923c57bf66952839813969a98692646256026833
wrzla <wrzla@ya.ru>
2025-04-27 13:15:02 +0300

    ProductService.cs

--- Занятие 4/Filters/ProductService.cs
+++ Занятие 4/Filters/ProductService.cs
@@ -1,20 +1,66 @@
-﻿namespace Filters
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Filters
 {
-	public static class ProductService
-	{
-		public static List<Product> FilterProducts(List<Product> products, Func<Product, bool> filter)
-		{
-			var filteredProducts = new List<Product>();
+    public static class ProductService
+    {
+        public static List<Product> FilterProducts(List<Product> products, Func<Product, bool> filter)
+        {
+            var filteredProducts = new List<Product>();
 
-			foreach (var product in products)
-			{
-				if (filter(product))
-					filteredProducts.Add(product);
-			}
+            foreach (var product in products)
+            {
+                if (filter(product))
+                    filteredProducts.Add(product);
+            }
 
-			return filteredProducts;
-		}
+            return filteredProducts;
+        }
+        // Метод Not
+        public static Func<Product, bool> Not(Func<Product, bool> filter)
+        {
+            return product => !filter(product);
+        }
+        // Метод And (логическое И)
+        public static Func<Product, bool> And(params Func<Product, bool>[] filters)
+        {
+            return product =>
+            {
+                foreach (var filter in filters)
+                {
+                    if (!filter(product)) return false; // Если хотя бы один фильтр не проходит, возвращаем false
+                }
+                return true;
+            };
+        }
+        // Метод Or (логическое ИЛИ)
+        public static Func<Product, bool> Or(params Func<Product, bool>[] filters)
+        {
+            return product =>
+            {
+                foreach (var filter in filters)
+                {
+                    if (filter(product)) return true; // Если хотя бы один фильтр проходит, возвращаем true
+                }
+                return false;
+            };
+        }
+        // Метод Xor (исключающее ИЛИ)
+        public static Func<Product, bool> Xor(params Func<Product, bool>[] filters)
+        {
+            return product =>
+            {
+                int trueCount = 0;
 
-		//напишите методы Not, And, Or и Xor
-	}
+                foreach (var filter in filters)
+                {
+                    if (filter(product)) trueCount++;
+                }
+
+                return trueCount == 1; // Должно быть ровно одно совпадение
+            };
+        }
+    }
 }
