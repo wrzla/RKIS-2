7cd0ebd47e62d029a0d89f2d6fa83ae6bd845ffa
wrzla <wrzla@ya.ru>
2025-04-27 13:01:25 +0300

    MovingMaxTask.cs

--- Занятие 2/Smooth/MovingMaxTask.cs
+++ Занятие 2/Smooth/MovingMaxTask.cs
@@ -2,13 +2,32 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
-namespace yield;
-
-public static class MovingMaxTask
+namespace yield
 {
-	public static IEnumerable<DataPoint> MovingMax(this IEnumerable<DataPoint> data, int windowWidth)
+	public static class MovingMaxTask
 	{
-		//Fix me!
-		return data;
+		public static IEnumerable<DataPoint> MovingMax(this IEnumerable<DataPoint> data, int windowWidth)
+		{
+			if (windowWidth <= 0)
+				throw new System.ArgumentOutOfRangeException();
+			int i = 1;
+			LinkedList<double> maxPotentials = new LinkedList<double>();
+			Queue<double> windowNumbers = new Queue<double>();
+			foreach (DataPoint dataPoint in data)
+			{
+				if (i <= windowWidth)
+					i++;
+				else if (maxPotentials.Count == 0)
+					windowNumbers.Dequeue();
+				else if (maxPotentials.First.Value == windowNumbers.Dequeue())
+					maxPotentials.RemoveFirst();
+				windowNumbers.Enqueue(dataPoint.OriginalY);
+				while (maxPotentials.Count > 0 && maxPotentials.Last.Value <= dataPoint.OriginalY)
+					maxPotentials.RemoveLast();
+				maxPotentials.AddLast(dataPoint.OriginalY);
+				var newDataPoint = dataPoint.WithMaxY(maxPotentials.First.Value);
+				yield return newDataPoint;
+			}
+		}
 	}
 }
