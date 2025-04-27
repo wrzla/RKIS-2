adcf682da81fd1d6ff8959158ccceff4ab2ae401
wrzla <wrzla@ya.ru>
2025-04-27 13:14:33 +0300

    ProductFilters.cs

--- Занятие 4/Filters/ProductFilters.cs
+++ Занятие 4/Filters/ProductFilters.cs
@@ -1,30 +1,32 @@
-﻿namespace Filters
+using System;
+
+namespace Filters
 {
-	public static class ProductFilters
-	{
-		public static Func<Product, bool> FilterByCategory(Category category)
-		{
-			return ...;
-		}
+    public static class ProductFilters
+    {
+        public static Func<Product, bool> FilterByCategory(Category category)
+        {
+            return product => product.Category == category;
+        }
 
-		public static Func<Product, bool> FilterByPrice(decimal minPrice, decimal maxPrice)
-		{
-			return ...;
-		}
+        public static Func<Product, bool> FilterByPrice(decimal minPrice, decimal maxPrice)
+        {
+            return product => product.Price >= minPrice && product.Price <= maxPrice;
+        }
 
-		public static Func<Product, bool> FilterByStock(int minStock)
-		{
-			return ...;
-		}
+        public static Func<Product, bool> FilterByStock(int minStock)
+        {
+            return product => product.StockCount >= minStock;
+        }
 
-		public static Func<Product, bool> FilterByExpirationDate(DateTime date)
-		{
-			return ...
-		}
+        public static Func<Product, bool> FilterByExpirationDate(DateTime currentDate)
+        {
+            return product => !product.ExpirationDate.HasValue ||  product.ProductionDate + product.ExpirationDate.Value >= currentDate;
+        }
 
-		public static Func<Product, bool> FilterByNameContains(string namePart)
-		{
-			return ...;
-		}
-	}
+        public static Func<Product, bool> FilterByNameContains(string namePart)
+        {
+            return product => product.Name.Contains(namePart, StringComparison.OrdinalIgnoreCase);
+        }
+    }
 }
