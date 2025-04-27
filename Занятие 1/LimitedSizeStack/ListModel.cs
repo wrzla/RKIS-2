b27e9449077b735830bf188a6272e34dcd9a985c
wrzla <wrzla@ya.ru>
2025-04-27 12:56:54 +0300

    ListModel.cs

--- Занятие 1/LimitedSizeStack/ListModel.cs
+++ Занятие 1/LimitedSizeStack/ListModel.cs
@@ -1,3 +1,4 @@
+
 ﻿using System;
 using System.Collections.Generic;
 
@@ -5,36 +6,50 @@ namespace LimitedSizeStack;
 
 public class ListModel<TItem>
 {
-	public List<TItem> Items { get; }
-	public int UndoLimit;
-        
-	public ListModel(int undoLimit) : this(new List<TItem>(), undoLimit)
-	{
-	}
+    public enum TypeAction { AddItem, RemoveItem }
 
-	public ListModel(List<TItem> items, int undoLimit)
-	{
-		Items = items;
-		UndoLimit = undoLimit;
-	}
+    public List<TItem> Items { get; }
 
-	public void AddItem(TItem item)
-	{
-		Items.Add(item);
-	}
+    public int UndoLimit { get; }
 
-	public void RemoveItem(int index)
-	{
-		Items.RemoveAt(index);
-	}
+    private LimitedSizeStack<(TypeAction Action, TItem Item, int Index)> History { get; }
 
-	public bool CanUndo()
-	{
-		return false;
-	}
+    public ListModel(List<TItem>? items, int undoLimit)
+    {
+        Items = items ?? new List<TItem>();
+        UndoLimit = undoLimit;
+        History = new LimitedSizeStack<(TypeAction, TItem, int)>(undoLimit);
+    }
 
-	public void Undo()
-	{
-		throw new NotImplementedException();
-	}
+    public ListModel(int undoLimit) : this(null, undoLimit) { }
+
+    public void AddItem(TItem item)
+    {
+        if (item == null) throw new ArgumentNullException(nameof(item));
+
+        History.Push((TypeAction.AddItem, item, Items.Count));
+        Items.Add(item);
+    }
+
+    public void RemoveItem(int index)
+    {
+        if (index < 0 || index >= Items.Count) throw new ArgumentOutOfRangeException(nameof(index));
+
+        History.Push((TypeAction.RemoveItem, Items[index], index));
+        Items.RemoveAt(index);
+    }
+
+    public bool CanUndo() => History.Count > 0;
+
+    public void Undo()
+    {
+        if (!CanUndo()) throw new InvalidOperationException("No actions to undo.");
+
+        var (action, item, index) = History.Pop();
+
+        if (action == TypeAction.AddItem)
+            Items.RemoveAt(index);
+        else
+            Items.Insert(index, item);
+    }
 }
