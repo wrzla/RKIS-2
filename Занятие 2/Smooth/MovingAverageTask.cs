eb7d06fbc6abdbe04e5dd6ec141ca87137b921f7
wrzla <wrzla@ya.ru>
2025-04-27 13:00:49 +0300

    MovingAverageTask.cs

--- Занятие 2/Smooth/MovingAverageTask.cs
+++ Занятие 2/Smooth/MovingAverageTask.cs
@@ -1,12 +1,29 @@
 using System.Collections.Generic;
 
-namespace yield;
-
-public static class MovingAverageTask
+namespace yield
 {
-	public static IEnumerable<DataPoint> MovingAverage(this IEnumerable<DataPoint> data, int windowWidth)
+	public static class MovingAverageTask
 	{
-		//Fix me!
-		return data;
+		public static IEnumerable<DataPoint> MovingAverage(this IEnumerable<DataPoint> data, int windowWidth)
+		{
+			if (windowWidth <= 0)
+				throw new System.ArgumentOutOfRangeException();
+			Queue<double> lastYs = new Queue<double>();
+			double sum = 0;
+			double result = 0;
+			foreach (var dataPoint in data)
+			{
+				if (lastYs.Count < windowWidth)
+				{
+					sum += dataPoint.OriginalY;
+					result = sum / (lastYs.Count + 1);
+				}
+				else
+					result += (dataPoint.OriginalY - lastYs.Dequeue()) / windowWidth;
+				var newDataPoint = dataPoint.WithAvgSmoothedY(result);
+				yield return newDataPoint;
+				lastYs.Enqueue(dataPoint.OriginalY);
+			}
+		}
 	}
 }
